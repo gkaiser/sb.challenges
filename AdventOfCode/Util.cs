2ac1e1f2aa4ce23323e91a66356e55a758b1241b
Guy Kaiser <grkaiser@gmail.com>
2015-12-07 21:04:28 -0500

    Adding 'Advent of Code'.

---
+++ AdventOfCode/Util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace AdventOfCode
+{
+  internal static class Util
+  {
+    public static string GetMD5Hash(string input)
+    {
+      var sb = new StringBuilder();
+
+      using (var md5 = System.Security.Cryptography.MD5.Create())
+      {
+        var inputBytes = Encoding.UTF8.GetBytes(input);
+        var hash = md5.ComputeHash(inputBytes);
+
+        for (int i = 0; i < hash.Length; i++)
+          sb.Append(hash[i].ToString("x2"));
+      }
+      
+      return sb.ToString();
+    }
+  }
+}
