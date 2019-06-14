c0d00c126de249e090884d6a50c9710580d3c240
Guy Kaiser <grkaiser@gmail.com>
2019-06-14 13:53:37 -0400

    More challenges

--- HackerRank/Arrays_ArrayManipulation.cs
+++ HackerRank/Arrays_ArrayManipulation.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HackerRank
 {
@@ -32,6 +30,7 @@ namespace HackerRank
     {
       Console.WriteLine("Manipulating array...");
 
+      /*
       var arr = new long[n];
 
       for (int i = 0; i < queries.Length; i++)
@@ -41,11 +40,54 @@ namespace HackerRank
         var idxR = opDat[1];
         var summ = opDat[2];
 
+        if (summ == 0)
+          continue;
+
         for (int j = idxL - 1; j < idxR; j++)
           arr[j] += summ;
       }
 
       return arr.Max();
+      */
+
+      var fake = new SortedDictionary<int, long>();
+
+      for (int i = 0; i < queries.Length; i++)
+      {
+        var ops = queries[i];
+        var idxL = ops[0];
+        var idxR = ops[1];
+        var summ = ops[2];
+
+        if (summ == 0)
+          continue;
+
+        if (!fake.ContainsKey(idxL))
+          fake.Add(idxL, summ);
+        else
+          fake[idxL] += summ;
+
+        if (idxR < n)
+        {
+          idxR++;
+          if (!fake.ContainsKey(idxR))
+            fake.Add(idxR, -1 * summ);
+          else
+            fake[idxR] -= summ;
+        }
+      }
+
+      var max = 0l;
+      var cur = 0l;
+
+      foreach (var mod in fake.Values)
+      {
+        cur += mod;
+        if (cur > max)
+          max = cur;
+      }
+
+      return max;
     }
   }
 }
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace HackerRank
 {
@@ -11,13 +12,92 @@ namespace HackerRank
       //Arrays_DynArray.Solve();
       //Arrays_LeftRotation.Solve();
       //Arrays_SparseArrays.Solve();
-      Arrays_ArrayManipulation.Solve();
+      //Arrays_ArrayManipulation.Solve(); /* STOLEN SOLUTION, MAKES 0 SENSE TO ME */
 
+      //Trees_PreorderTraversal.Solve();
 
+      //var arr = new int[4][];
+      //arr[0] = new[] { 01, 02, 03, 04 };
+      //arr[1] = new[] { 05, 06, 07, 09 };
+      //arr[2] = new[] { 09, 10, 11, 12 };
+      //arr[3] = new[] { 13, 14, 15, 16 };
+      //var arr = new[] { 1, 1, 0, -1, -1 };
+
+      //Program.plusMinus(arr);
+      //Program.staircase(6);
+      //Program.miniMaxSum(new[] { 1, 3, 5, 7, 9 });
+      //Program.miniMaxSum(new[] { 256741038, 623958417, 467905213, 714532089, 938071625 });
+      //Program.birthdayCakeCandles(new[] { 4, 4, 1, 3 });
 
       Console.WriteLine();
       Console.Write("Done, press [ENTER] to quit... ");
       Console.ReadLine();
+    }
+
+    public static int dD(int[][] arr)
+    {
+      // 01 + 06 + 11 + 16 = 34
+      // 04 + 07 + 10 + 13 = 34
+
+      var dp = 0;
+      for (int i = 0; i < arr.Length; i++)
+      {
+        dp += arr[i][i];
+      }
+
+      var ds = 0;
+      for (int i = arr.Length - 1; i >= 0; i--)
+      {
+        ds += arr[arr.Length - 1 - i][i];
+      }
+
+      Console.WriteLine(Math.Abs(dp - ds));
+
+      return Math.Abs(dp - ds);
+    }
+
+    public static void plusMinus(int[] arr)
+    {
+      var l = (decimal)arr.Length;
+      var pos = (decimal)arr.Where(i => i > 0).Count();
+      var neg = (decimal)arr.Where(i => i < 0).Count();
+      var zer = (decimal)arr.Where(i => i == 0).Count();
+
+      Console.WriteLine((pos / l).ToString("0.000000"));
+      Console.WriteLine((neg / l).ToString("0.000000"));
+      Console.WriteLine((zer / l).ToString("0.000000"));
+    }
+
+    static void staircase(int n)
+    {
+      for (int i = 1; i <= n; i++)
+      {
+        var str = new string('#', i).PadLeft(n);
+        Console.WriteLine(str);
+      }
+    }
+
+    static void miniMaxSum(int[] arr)
+    {
+      Array.Sort(arr);
+
+      long min = (long)arr[0] + (long)arr[1] + (long)arr[2] + (long)arr[3];
+      long max = (long)arr[arr.Length - 1] + (long)arr[arr.Length - 2] + (long)arr[arr.Length - 3] + (long)arr[arr.Length - 4];
+
+      Console.Write(min);
+      Console.Write(" ");
+      Console.Write(max);
+      Console.WriteLine();
+    }
+
+    static int birthdayCakeCandles(int[] ar)
+    {
+      var max = ar.Max();
+      var ct = ar.Where(a => a == max).Count();
+
+      //Console.WriteLine(ct);
+
+      return ct;
     }
 
   }
