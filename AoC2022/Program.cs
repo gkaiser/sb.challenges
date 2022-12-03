763f0a5547ab59e197f70881e7323f5a6adb5352
Guy Kaiser <grkaiser@gmail.com>
2022-12-03 11:41:49 -0500

    Complete day 3

---
+++ AoC2022/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AoC2022
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //Day01.SolvePart1();
+      //Day01.SolvePart2();
+      //Day02.SolvePart1();
+      //Day02.SolvePart2();
+      //Day03.SolvePart1();
+      Day03.SolvePart2();
+      if (System.Diagnostics.Debugger.IsAttached)
+      {
+        Console.WriteLine();
+        Console.Write($"Done, press [ENTER] to quit... ");
+        Console.ReadLine();
+      }
+    }
+
+  }
+}
