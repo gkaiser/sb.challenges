12066c16bce7f3de8504347fcfe4594b705bb416
Guy Kaiser <grkaiser@gmail.com>
2023-12-03 19:23:02 -0500

    Finished AoC 2023 through Day 03

---
+++ AoC2023/Day01.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoC2023
+{
+  internal static class Day01
+  {
+    internal static void SolvePart1()
+    {
+      //var inp = new[]
+      //{
+      //  "1abc2",
+      //  "pqr3stu8vwx",
+      //  "a1b2c3d4e5f",
+      //  "treb7uchet",
+      //};
+      var inp = System.IO.File.ReadAllLines("Day01.txt");
+
+      var s = 0;
+
+      foreach (var l in inp)
+      {
+        var ns = "";
+
+        var ca = l.Where(c => char.IsDigit(c)).ToArray();
+        if (ca.Length == 1)
+          ca = new[] { ca[0], ca[0] };
+        else if (ca.Length > 2)
+          ca = new[] { ca[0], ca[ca.Length - 1] };
+
+        var cs = new string(ca);
+
+        s += int.Parse(cs);
+      }
+
+      Console.WriteLine($"{s}");
+    }
+
+    internal static void SolvePart2()
+    {
+      //var inp = new[]
+      //{
+      //  "two1nine",
+      //  "eightwothree",
+      //  "abcone2threexyz",
+      //  "xtwone3four",
+      //  "4nineeightseven2",
+      //  "zoneight234",
+      //  "7pqrstsixteen",
+      //};
+      var inp = System.IO.File.ReadAllLines("Day01.txt");
+
+      var dictNums = new Dictionary<string, int>
+      {
+        { "one", 1 },
+        { "two", 2 },
+        { "three", 3 },
+        { "four", 4 },
+        { "five", 5 },
+        { "six", 6 },
+        { "seven", 7 },
+        { "eight", 8 },
+        { "nine", 9 },
+      };
+
+      var s = 0;
+
+      foreach (var l in inp)
+      {
+        var ns = "";
+
+        for (int j = 0; j < l.Length; j++)
+        {
+          if (char.IsDigit(l[j]))
+          {
+            ns += $"{l[j]}";
+            continue;
+          }
+
+          var foundWord = false;
+          foreach (var k in dictNums.Keys)
+          {
+            if (j + k.Length > l.Length)
+              continue;
+
+            var lv = l.Substring(j, k.Length);
+            if (lv == k)
+            {
+              ns += $"{dictNums[k]}";
+              foundWord = true;
+              break;
+            }
+          }
+
+          if (!foundWord)
+            ns += $"{l[j]}";
+        }
+
+        var ca = ns.Where(c => char.IsDigit(c)).ToArray();
+        if (ca.Length == 1)
+          ca = new[] { ca[0], ca[0] };
+        else if (ca.Length > 2)
+          ca = new[] { ca[0], ca[ca.Length - 1] };
+
+        var cs = new string(ca);
+
+        s += int.Parse(cs);
+      }
+
+      // 55427 low
+      // 55429 correct (modifying the original string wasn't the way to go)
+
+      Console.WriteLine($"{s}");
+    }
+
+  }
+}
---
+++ AoC2023/Pt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace AoC2023
+{
+  internal class Pt
+  {
+    public int X;
+    public int Y;
+
+    public Pt(int x, int y)
+    {
+      this.X = x;
+      this.Y = y;
+    }
+
+    public override string ToString() => $"{this.X}, {this.Y}";
+
+    public static bool operator ==(Pt p1, Pt p2)
+    {
+      if (object.ReferenceEquals(p1, p2))
+        return true;
+      if (p1 is null || p2 is null)
+        return false;
+
+      return p1.X == p2.X && p1.Y == p2.Y;
+    }
+
+    public static bool operator !=(Pt p1, Pt p2) => !(p1 == p2);
+
+    public override bool Equals(object? obj) => this == (obj as Pt);
+
+    public override int GetHashCode()
+    {
+      unchecked
+      {
+        var hc = 1455329;
+        hc = hc * 2235067 ^ this.X.GetHashCode();
+        hc = hc * 2235067 ^ this.Y.GetHashCode();
+        return hc;
+      }
+    }
+
+  }
+}
