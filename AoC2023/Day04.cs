a85521afc3b6669894c2651712c6b8b853f6cfa8
Guy Kaiser <grkaiser@gmail.com>
2024-01-13 16:18:21 -0500

    AoC2023 stuff, failure, etc

--- AoC2023/Day04.cs
+++ AoC2023/Day04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC2023
 {
@@ -6,22 +8,100 @@ namespace AoC2023
   {
     internal static void SolvePart1()
     {
-      var inp = new[]
-      {
-        ""
-      };
+      //var inp = new[]
+      //{
+      //  "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+      //  "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+      //  "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+      //  "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+      //  "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+      //  "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+      //};
+      var inp = System.IO.File.ReadAllLines("Day04.txt");
 
-      Console.WriteLine("p1...");
+      var pt = 0;
+
+      foreach (var l in inp)
+      {
+        var vals = l.Substring(l.IndexOf(":") + 1);
+        var wnums = vals.Split('|')[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(wn => int.Parse(wn));
+        var pnums = vals.Split('|')[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(pn => int.Parse(pn));
+
+        var winCt = wnums.Intersect(pnums).Count();
+
+        pt += (int)Math.Pow(2, winCt - 1);
+      }
+
+      Console.WriteLine($"p1 {pt}...");
     }
-    
+
     internal static void SolvePart2()
     {
-      var inp = new[]
-      {
-        ""
-      };
+      //var inp = new[]
+      //{
+      //  "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+      //  "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+      //  "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+      //  "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+      //  "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+      //  "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+      //};
+      var inp = System.IO.File.ReadAllLines("Day04.txt");
 
-      Console.WriteLine("p2...");
+      var sw = System.Diagnostics.Stopwatch.StartNew();
+
+      var inpNums = new int[inp.Length][];
+      for (int i = 0; i < inp.Length; i++)
+      {
+        var l = inp[i];
+        var vals = l.Substring(l.IndexOf(":") + 1);
+        var wnums = vals.Split('|')[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(wn => int.Parse(wn)).ToArray();
+        var pnums = vals.Split('|')[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(pn => int.Parse(pn)).ToArray();
+
+        var dest = new int[1 + wnums.Length + pnums.Length];
+
+        dest[0] = wnums.Length;
+        Array.ConstrainedCopy(wnums, 0, dest, 0 + 1, wnums.Length);
+        Array.ConstrainedCopy(pnums, 0, dest, wnums.Length + 1, pnums.Length);
+
+        inpNums[i] = dest;
+      }
+
+      var ct = 0;
+
+      for (int i = 0; i < inp.Length; i++)
+      {
+        var cards = new List<int> { i };
+
+        ct++;
+
+        for (int j = 0; j < cards.Count; j++)
+        {
+          var anums = inpNums[cards[j]];
+          var wnums = new int[anums[0]];
+          var pnums = new int[anums.Length - 1 - anums[0]];
+
+          Array.ConstrainedCopy(anums, 1, wnums, 0, wnums.Length);
+          Array.ConstrainedCopy(anums, 1 + wnums.Length, pnums, 0, pnums.Length);
+
+          var winct = wnums.Intersect(pnums).Count();
+
+          while (winct > 0)
+          {
+            cards.Add(cards[j] + winct);
+
+            ct++;
+
+            winct--;
+          }
+        }
+      }
+
+      sw.Stop();
+
+      // Sum is 6874754 (00:00:16.3818233)...
+      // Sum is 6874754 (00:00:03.1640473)...
+      Console.WriteLine($"Sum is {ct} ({sw.Elapsed})...");
     }
 
   }
---
+++ AoC2023/Day05.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AoC2023
+{
+  internal class Day05
+  {
+    internal static void SolvePart1()
+    {
+      var inp = new[]
+      {
+        "seeds: 98 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4",
+      };
+      //var inp = System.IO.File.ReadAllLines("Day05.txt");
+
+      var seeds = inp[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse).ToArray();
+
+      var maps = new List<Tuple<ulong, ulong, ulong>>();
+
+      for (int i = 3; i < inp.Length; i++)
+      {
+        while (inp[i] != "")
+        {
+          var vals = inp[i].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+          maps.Add(new Tuple<ulong, ulong, ulong>(ulong.Parse(vals[0]), ulong.Parse(vals[1]), ulong.Parse(vals[2])));
+        }
+
+        // now we're on the blank line, skip to next num-line
+        i += 2;
+
+        // process the seeds with the latest maps
+
+        for (int j = 0; j < seeds.Length; j++)
+        {
+          if (seeds[j] < src)
+            continue;
+          if (seeds[j] < src + rng)
+          {
+            //          50    98      98
+            seeds[j] = dst + (src - seeds[j]);
+            continue;
+          }
+        }
+      }
+
+      Console.WriteLine("p1...");
+    }
+
+    internal static void SolvePart2()
+    {
+      var inp = new[]
+      {
+        ""
+      };
+
+      Console.WriteLine("p2...");
+    }
+
+  }
+}
