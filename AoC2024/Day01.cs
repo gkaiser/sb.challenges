f50e81fc85ea49ccad49c73cdcb6f9f87cb7c559
Guy Kaiser <grkaiser@gmail.com>
2024-12-01 09:44:53 -0500

    Add AoC2024 (woooooooooooooo!)

---
+++ AoC2024/Day01.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AoC2024
+{
+  internal static class Day01
+  {
+    internal static void SolvePart1()
+    {
+      //var inp = new []
+      //{
+      //  "3   4",
+      //  "4   3",
+      //  "2   5",
+      //  "1   3",
+      //  "3   9",
+      //  "3   3",
+      //};
+      var inp = File.ReadAllLines("Day01.txt");
+
+      var ll = new List<int>();
+      var lr = new List<int>();
+
+      foreach (var l in inp)
+      {
+        var v = l.Split(' ');
+
+        ll.Add(int.Parse(v.First()));
+        lr.Add(int.Parse(v.Last()));
+      }
+
+      ll.Sort();
+      lr.Sort();
+
+      var dist = 0;
+      for (int i = 0; i < ll.Count; i++)
+      {
+        dist += Math.Abs(ll[i] - lr[i]);
+      }
+
+      Console.WriteLine(dist);
+
+      
+    }
+
+    internal static void SolvePart2()
+    {
+      //var inp = new []
+      //{
+      //  "3   4",
+      //  "4   3",
+      //  "2   5",
+      //  "1   3",
+      //  "3   9",
+      //  "3   3",
+      //};
+      var inp = File.ReadAllLines("Day01.txt");
+
+      var ll = new List<int>();
+      var lr = new List<int>();
+
+      foreach (var l in inp)
+      {
+        var v = l.Split(' ');
+
+        ll.Add(int.Parse(v.First()));
+        lr.Add(int.Parse(v.Last()));
+      }
+
+      var siml = 0;
+      for (int i = 0; i < ll.Count; i++)
+      {
+        siml += ll[i] * lr.Count(lrv => lrv == ll[i]);
+      }
+
+      Console.WriteLine(siml);
+    }
+
+  }
+}
---
+++ AoC2024/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AoC2024
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //Day01.SolvePart1();
+      //Day01.SolvePart2();
+
+      if (System.Diagnostics.Debugger.IsAttached)
+      {
+        Console.WriteLine();
+        Console.Write("Done, press [ENTER] to quit...");
+        Console.ReadLine();
+      }
+    }
+
+  }
+}
